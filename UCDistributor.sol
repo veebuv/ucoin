483f6aee86aed057ef0f81ca76425f9b4ba9fd1a
Venkatesh Namburi <vnamburi92@gmail.com>
2018-03-22 02:04:51 +1100

    init commit

---
+++ UCDistributor.sol
@@ -0,0 +1,56 @@
+
+pragma solidity ^0.4.18;
+
+import './Ownable.sol';
+import './UCoin.sol';
+// contract interfaceUNV {
+//     function totalSupply() public constant returns (uint);
+//     function balanceOf(address tokenOwner) public constant returns (uint balance);
+//     function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+//     function transfer(address to, uint tokens) public returns (bool success);
+//     function approve(address spender, uint tokens) public returns (bool success);
+//     function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    
+//     function increaseApproval(address _spender, uint _addedValue) public returns (bool);
+//     function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool);
+//     function burn(uint256 _value) public;
+
+//     event Transfer(address indexed from, address indexed to, uint tokens);
+//     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+//     event Burn(address indexed burner, uint256 value);
+//     event Logger(address sender);
+// }
+
+contract UCDistributor is Ownable {
+    
+    UCoin dc;
+    
+    function UCDistributor(address _t) public {
+        dc = UCoin(_t);
+    }
+    
+    function totalSupply ()
+    onlyOwner
+    public view returns (uint256){
+        uint256 sup = dc.totalSupply();
+        emit Logger("UCD", sup);
+        return sup;
+    }
+    
+    function transfer ( address tokenAddress, address addy, uint256 total)
+    public {
+        UCoin(tokenAddress).transfer(addy, total);
+    }
+    
+    function balanceOf (address addy)
+    onlyOwner
+    public view returns (uint256){
+        uint256 balance = dc.balanceOf(addy);
+        emit Logger("Balance", balance);
+        return balance;
+    }
+    
+    event Logger(string someText, uint256 amount);
+    event LoggerAdd(string someText, address addy);
+
+}
---
+++ UCoin.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.4.18;
+
+import './BaseToken.sol';
+
+contract Ucoin is BaseToken {
+  // Publicly listed name
+  string public name = "U COIN";
+  // Symbol under which token will be trading
+  string public symbol = "U";
+  // 1 ETH consists of 10^18 Wei, which is the smallest ETH unit
+  uint8 public decimals = 18;
+  // Defining the value of a million for easy calculations - order of declaration matters (hoisting)
+  uint256 million = 1000000 * (uint256(10) ** decimals);
+  // We are offering a total of 100 Million Ulcoin Tokens to distribute
+  uint256 public totalSupply = 40000 * million;
+  // Address where all the tokens are held, as tokens aren't held within the Smart Contract
+  address public masterWallet;
+
+  // constructor function
+  function Ucoin()
+    public
+  {
+    // The wallet from which the contract is deployed, also the owner of the contract
+    owner = msg.sender;
+    masterWallet = owner;
+
+
+    // Assign total supply to master wallet
+    // https://github.com/OpenZeppelin/zeppelin-solidity/issues/494
+    // A token contract which creates new tokens SHOULD trigger a Transfer event with the _from address set to 0x0 when tokens are created.
+    balances[masterWallet] = totalSupply;
+    emit Transfer(0x0, masterWallet, totalSupply);
+  }
+
+  function burn(uint256 _value) onlyOwner public {
+    require(_value <= balances[msg.sender]);
+    // no need to require value <= totalSupply, since that would imply the
+    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+    address burner = msg.sender;
+    balances[burner] = balances[burner].sub(_value);
+    totalSupply_ = totalSupply_.sub(_value);
+    emit Burn(burner, _value);
+    emit Transfer(burner, address(0), _value);
+  }
+
+  // Events
+  event Burn(address indexed burner, uint256 value);
+}
